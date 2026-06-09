3f10fc01e9c7c7c229d9da58fa16070f74f24322
brundhap <brundhadharani@gmail.com>
2026-06-09 12:32:06 +0530

    Create switch.js

---
+++ switch.js
@@ -0,0 +1,48 @@
+let score = 90;
+
+switch (true) { // >, <, ==, && it case tha long jains
+    case (score >= 90):
+        console.log("Grade: A");
+        break;
+    case (score >= 80):
+        console.log("Grade: B");
+        break;
+    case (score >= 70):
+        console.log("Grade: C");    
+        break;
+    default:
+        console.log("Grade: d");
+}
+
+
+
+
+const number=2 //its normal case 
+switch(number){
+    case 1:
+     console.log("one number");
+     break;
+    case 2:
+        console.log("two number")
+        break;
+    default:
+    console.log("this tha defalut value")
+}
+
+
+const role = "guest";
+
+switch (role) {
+    case "admin":
+        console.log("Show Admin Dashboard");
+        break;
+    case "teacher":
+        console.log("Show Teacher Dashboard");
+        break;
+    case "student":
+        console.log("Show Student Dashboard");
+        break;
+    default:
+        console.log("Show Access Denied"); 
+}
+
